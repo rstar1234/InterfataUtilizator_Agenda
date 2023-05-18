51b61cf3cf6c3191cba90537724a4a41d75da75a
raluca_stamate <raluca.stamate@student.usv.ro>
2023-05-18 03:08:11 +0300

    Am adaugat controale pentru adaugarea unei persoane si am implementat aceasta optiune.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -34,74 +34,255 @@
             this.lblEmail = new System.Windows.Forms.Label();
             this.lblNumarDeTelefon = new System.Windows.Forms.Label();
             this.lblZiDeNastere = new System.Windows.Forms.Label();
+            this.lblAddNume = new System.Windows.Forms.Label();
+            this.lblAddZiDeNastere = new System.Windows.Forms.Label();
+            this.lblAddNumărDeTelefon = new System.Windows.Forms.Label();
+            this.lblAddEmail = new System.Windows.Forms.Label();
+            this.lblAddGrup = new System.Windows.Forms.Label();
+            this.txtAddNume = new System.Windows.Forms.TextBox();
+            this.txtAddNumarDeTelefon = new System.Windows.Forms.TextBox();
+            this.txtAddEmail = new System.Windows.Forms.TextBox();
+            this.checkPrieteni = new System.Windows.Forms.CheckBox();
+            this.checkFamilie = new System.Windows.Forms.CheckBox();
+            this.checkServiciu = new System.Windows.Forms.CheckBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnAfiseaza = new System.Windows.Forms.Button();
+            this.dateTimeZiDeNastere = new System.Windows.Forms.DateTimePicker();
             this.SuspendLayout();
             // 
             // lbITitlu
             // 
-            this.lbITitlu.BackColor = System.Drawing.Color.DarkRed;
-            this.lbITitlu.ForeColor = System.Drawing.Color.Gainsboro;
-            this.lbITitlu.Location = new System.Drawing.Point(130, 32);
+            this.lbITitlu.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.lbITitlu.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.lbITitlu.Location = new System.Drawing.Point(599, 33);
             this.lbITitlu.Name = "lbITitlu";
-            this.lbITitlu.Size = new System.Drawing.Size(899, 33);
+            this.lbITitlu.Size = new System.Drawing.Size(1032, 33);
             this.lbITitlu.TabIndex = 0;
             this.lbITitlu.Text = "Agenda";
             this.lbITitlu.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lbITitlu.Visible = false;
+            this.lbITitlu.VisibleChanged += new System.EventHandler(this.Lbl_VisibleChanged);
             // 
             // lblNume
             // 
             this.lblNume.AutoSize = true;
-            this.lblNume.ForeColor = System.Drawing.Color.Indigo;
-            this.lblNume.Location = new System.Drawing.Point(126, 100);
+            this.lblNume.ForeColor = System.Drawing.Color.DeepSkyBlue;
+            this.lblNume.Location = new System.Drawing.Point(599, 100);
             this.lblNume.Name = "lblNume";
             this.lblNume.Size = new System.Drawing.Size(51, 20);
             this.lblNume.TabIndex = 1;
             this.lblNume.Text = "Nume";
+            this.lblNume.Visible = false;
+            this.lblNume.VisibleChanged += new System.EventHandler(this.Lbl_VisibleChanged);
             // 
             // lblGrup
             // 
             this.lblGrup.AutoSize = true;
-            this.lblGrup.ForeColor = System.Drawing.Color.Indigo;
-            this.lblGrup.Location = new System.Drawing.Point(984, 100);
+            this.lblGrup.ForeColor = System.Drawing.Color.DeepSkyBlue;
+            this.lblGrup.Location = new System.Drawing.Point(1489, 100);
             this.lblGrup.Name = "lblGrup";
             this.lblGrup.Size = new System.Drawing.Size(45, 20);
             this.lblGrup.TabIndex = 2;
             this.lblGrup.Text = "Grup";
+            this.lblGrup.Visible = false;
+            this.lblGrup.VisibleChanged += new System.EventHandler(this.Lbl_VisibleChanged);
             // 
             // lblEmail
             // 
             this.lblEmail.AutoSize = true;
-            this.lblEmail.ForeColor = System.Drawing.Color.Indigo;
-            this.lblEmail.Location = new System.Drawing.Point(805, 100);
+            this.lblEmail.ForeColor = System.Drawing.Color.DeepSkyBlue;
+            this.lblEmail.Location = new System.Drawing.Point(1281, 100);
             this.lblEmail.Name = "lblEmail";
             this.lblEmail.Size = new System.Drawing.Size(48, 20);
             this.lblEmail.TabIndex = 3;
             this.lblEmail.Text = "Email";
+            this.lblEmail.Visible = false;
+            this.lblEmail.VisibleChanged += new System.EventHandler(this.Lbl_VisibleChanged);
             // 
             // lblNumarDeTelefon
             // 
             this.lblNumarDeTelefon.AutoSize = true;
-            this.lblNumarDeTelefon.ForeColor = System.Drawing.Color.Indigo;
-            this.lblNumarDeTelefon.Location = new System.Drawing.Point(543, 100);
+            this.lblNumarDeTelefon.ForeColor = System.Drawing.Color.DeepSkyBlue;
+            this.lblNumarDeTelefon.Location = new System.Drawing.Point(1018, 100);
             this.lblNumarDeTelefon.Name = "lblNumarDeTelefon";
             this.lblNumarDeTelefon.Size = new System.Drawing.Size(131, 20);
             this.lblNumarDeTelefon.TabIndex = 4;
             this.lblNumarDeTelefon.Text = "Număr de telefon";
+            this.lblNumarDeTelefon.Visible = false;
+            this.lblNumarDeTelefon.VisibleChanged += new System.EventHandler(this.Lbl_VisibleChanged);
             // 
             // lblZiDeNastere
             // 
             this.lblZiDeNastere.AutoSize = true;
-            this.lblZiDeNastere.ForeColor = System.Drawing.Color.Indigo;
-            this.lblZiDeNastere.Location = new System.Drawing.Point(308, 100);
+            this.lblZiDeNastere.ForeColor = System.Drawing.Color.DeepSkyBlue;
+            this.lblZiDeNastere.Location = new System.Drawing.Point(782, 100);
             this.lblZiDeNastere.Name = "lblZiDeNastere";
             this.lblZiDeNastere.Size = new System.Drawing.Size(104, 20);
             this.lblZiDeNastere.TabIndex = 5;
             this.lblZiDeNastere.Text = "Zi de Nastere";
+            this.lblZiDeNastere.Visible = false;
+            this.lblZiDeNastere.VisibleChanged += new System.EventHandler(this.Lbl_VisibleChanged);
+            // 
+            // lblAddNume
+            // 
+            this.lblAddNume.AutoSize = true;
+            this.lblAddNume.ForeColor = System.Drawing.Color.LightSeaGreen;
+            this.lblAddNume.Location = new System.Drawing.Point(50, 46);
+            this.lblAddNume.Name = "lblAddNume";
+            this.lblAddNume.Size = new System.Drawing.Size(51, 20);
+            this.lblAddNume.TabIndex = 6;
+            this.lblAddNume.Text = "Nume";
+            // 
+            // lblAddZiDeNastere
+            // 
+            this.lblAddZiDeNastere.AutoSize = true;
+            this.lblAddZiDeNastere.ForeColor = System.Drawing.Color.LightSeaGreen;
+            this.lblAddZiDeNastere.Location = new System.Drawing.Point(50, 119);
+            this.lblAddZiDeNastere.Name = "lblAddZiDeNastere";
+            this.lblAddZiDeNastere.Size = new System.Drawing.Size(104, 20);
+            this.lblAddZiDeNastere.TabIndex = 7;
+            this.lblAddZiDeNastere.Text = "Zi de Nastere";
+            // 
+            // lblAddNumărDeTelefon
+            // 
+            this.lblAddNumărDeTelefon.AutoSize = true;
+            this.lblAddNumărDeTelefon.ForeColor = System.Drawing.Color.LightSeaGreen;
+            this.lblAddNumărDeTelefon.Location = new System.Drawing.Point(50, 192);
+            this.lblAddNumărDeTelefon.Name = "lblAddNumărDeTelefon";
+            this.lblAddNumărDeTelefon.Size = new System.Drawing.Size(131, 20);
+            this.lblAddNumărDeTelefon.TabIndex = 8;
+            this.lblAddNumărDeTelefon.Text = "Număr de telefon";
+            // 
+            // lblAddEmail
+            // 
+            this.lblAddEmail.AutoSize = true;
+            this.lblAddEmail.ForeColor = System.Drawing.Color.LightSeaGreen;
+            this.lblAddEmail.Location = new System.Drawing.Point(50, 265);
+            this.lblAddEmail.Name = "lblAddEmail";
+            this.lblAddEmail.Size = new System.Drawing.Size(48, 20);
+            this.lblAddEmail.TabIndex = 9;
+            this.lblAddEmail.Text = "Email";
+            // 
+            // lblAddGrup
+            // 
+            this.lblAddGrup.AutoSize = true;
+            this.lblAddGrup.ForeColor = System.Drawing.Color.LightSeaGreen;
+            this.lblAddGrup.Location = new System.Drawing.Point(50, 338);
+            this.lblAddGrup.Name = "lblAddGrup";
+            this.lblAddGrup.Size = new System.Drawing.Size(45, 20);
+            this.lblAddGrup.TabIndex = 10;
+            this.lblAddGrup.Text = "Grup";
+            // 
+            // txtAddNume
+            // 
+            this.txtAddNume.Location = new System.Drawing.Point(225, 46);
+            this.txtAddNume.Name = "txtAddNume";
+            this.txtAddNume.Size = new System.Drawing.Size(298, 26);
+            this.txtAddNume.TabIndex = 11;
+            // 
+            // txtAddNumarDeTelefon
+            // 
+            this.txtAddNumarDeTelefon.Location = new System.Drawing.Point(225, 202);
+            this.txtAddNumarDeTelefon.Name = "txtAddNumarDeTelefon";
+            this.txtAddNumarDeTelefon.Size = new System.Drawing.Size(298, 26);
+            this.txtAddNumarDeTelefon.TabIndex = 13;
+            // 
+            // txtAddEmail
+            // 
+            this.txtAddEmail.Location = new System.Drawing.Point(225, 280);
+            this.txtAddEmail.Name = "txtAddEmail";
+            this.txtAddEmail.Size = new System.Drawing.Size(298, 26);
+            this.txtAddEmail.TabIndex = 14;
+            // 
+            // checkPrieteni
+            // 
+            this.checkPrieteni.AutoSize = true;
+            this.checkPrieteni.ForeColor = System.Drawing.Color.LightSeaGreen;
+            this.checkPrieteni.Location = new System.Drawing.Point(225, 338);
+            this.checkPrieteni.Name = "checkPrieteni";
+            this.checkPrieteni.Size = new System.Drawing.Size(88, 24);
+            this.checkPrieteni.TabIndex = 15;
+            this.checkPrieteni.Text = "Prieteni";
+            this.checkPrieteni.UseVisualStyleBackColor = true;
+            this.checkPrieteni.CheckedChanged += new System.EventHandler(this.CheckGrupuri_CheckedChange);
+            // 
+            // checkFamilie
+            // 
+            this.checkFamilie.AutoSize = true;
+            this.checkFamilie.BackColor = System.Drawing.SystemColors.Control;
+            this.checkFamilie.ForeColor = System.Drawing.Color.LightSeaGreen;
+            this.checkFamilie.Location = new System.Drawing.Point(380, 338);
+            this.checkFamilie.Name = "checkFamilie";
+            this.checkFamilie.Size = new System.Drawing.Size(85, 24);
+            this.checkFamilie.TabIndex = 16;
+            this.checkFamilie.Text = "Familie";
+            this.checkFamilie.UseVisualStyleBackColor = false;
+            this.checkFamilie.CheckedChanged += new System.EventHandler(this.CheckGrupuri_CheckedChange);
+            // 
+            // checkServiciu
+            // 
+            this.checkServiciu.AutoSize = true;
+            this.checkServiciu.ForeColor = System.Drawing.Color.LightSeaGreen;
+            this.checkServiciu.Location = new System.Drawing.Point(225, 410);
+            this.checkServiciu.Name = "checkServiciu";
+            this.checkServiciu.Size = new System.Drawing.Size(90, 24);
+            this.checkServiciu.TabIndex = 17;
+            this.checkServiciu.Text = "Serviciu";
+            this.checkServiciu.UseVisualStyleBackColor = true;
+            this.checkServiciu.CheckedChanged += new System.EventHandler(this.CheckGrupuri_CheckedChange);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.ForeColor = System.Drawing.Color.LightSeaGreen;
+            this.btnAdd.Location = new System.Drawing.Point(225, 471);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(97, 35);
+            this.btnAdd.TabIndex = 18;
+            this.btnAdd.Text = "Adaugă";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.BtnAdd_Click);
+            // 
+            // btnAfiseaza
+            // 
+            this.btnAfiseaza.ForeColor = System.Drawing.Color.LightSeaGreen;
+            this.btnAfiseaza.Location = new System.Drawing.Point(380, 471);
+            this.btnAfiseaza.Name = "btnAfiseaza";
+            this.btnAfiseaza.Size = new System.Drawing.Size(85, 35);
+            this.btnAfiseaza.TabIndex = 19;
+            this.btnAfiseaza.Text = "Afișează";
+            this.btnAfiseaza.UseVisualStyleBackColor = true;
+            this.btnAfiseaza.Click += new System.EventHandler(this.BtnAfiseaza_Click);
+            // 
+            // dateTimeZiDeNastere
+            // 
+            this.dateTimeZiDeNastere.CalendarForeColor = System.Drawing.Color.DarkSlateGray;
+            this.dateTimeZiDeNastere.CalendarMonthBackground = System.Drawing.Color.DeepSkyBlue;
+            this.dateTimeZiDeNastere.CalendarTitleBackColor = System.Drawing.Color.SteelBlue;
+            this.dateTimeZiDeNastere.CalendarTitleForeColor = System.Drawing.Color.DarkSlateGray;
+            this.dateTimeZiDeNastere.Location = new System.Drawing.Point(225, 119);
+            this.dateTimeZiDeNastere.Name = "dateTimeZiDeNastere";
+            this.dateTimeZiDeNastere.Size = new System.Drawing.Size(298, 26);
+            this.dateTimeZiDeNastere.TabIndex = 20;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1356, 450);
+            this.ClientSize = new System.Drawing.Size(1680, 544);
+            this.Controls.Add(this.dateTimeZiDeNastere);
+            this.Controls.Add(this.btnAfiseaza);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.checkServiciu);
+            this.Controls.Add(this.checkFamilie);
+            this.Controls.Add(this.checkPrieteni);
+            this.Controls.Add(this.txtAddEmail);
+            this.Controls.Add(this.txtAddNumarDeTelefon);
+            this.Controls.Add(this.txtAddNume);
+            this.Controls.Add(this.lblAddGrup);
+            this.Controls.Add(this.lblAddEmail);
+            this.Controls.Add(this.lblAddNumărDeTelefon);
+            this.Controls.Add(this.lblAddZiDeNastere);
+            this.Controls.Add(this.lblAddNume);
             this.Controls.Add(this.lblZiDeNastere);
             this.Controls.Add(this.lblNumarDeTelefon);
             this.Controls.Add(this.lblEmail);
@@ -109,7 +290,8 @@
             this.Controls.Add(this.lblNume);
             this.Controls.Add(this.lbITitlu);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Agenda";
+            this.Click += new System.EventHandler(this.Form1_Click);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -123,6 +305,20 @@
         private System.Windows.Forms.Label lblEmail;
         private System.Windows.Forms.Label lblNumarDeTelefon;
         private System.Windows.Forms.Label lblZiDeNastere;
+        private System.Windows.Forms.Label lblAddNume;
+        private System.Windows.Forms.Label lblAddZiDeNastere;
+        private System.Windows.Forms.Label lblAddNumărDeTelefon;
+        private System.Windows.Forms.Label lblAddEmail;
+        private System.Windows.Forms.Label lblAddGrup;
+        private System.Windows.Forms.TextBox txtAddNume;
+        private System.Windows.Forms.TextBox txtAddNumarDeTelefon;
+        private System.Windows.Forms.TextBox txtAddEmail;
+        private System.Windows.Forms.CheckBox checkPrieteni;
+        private System.Windows.Forms.CheckBox checkFamilie;
+        private System.Windows.Forms.CheckBox checkServiciu;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnAfiseaza;
+        private System.Windows.Forms.DateTimePicker dateTimeZiDeNastere;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Configuration;
@@ -6,6 +7,7 @@ using System.Data;
 using System.Drawing;
 using System.IO;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -16,6 +18,14 @@ namespace InterfataUtilizator_Agenda
 {
     public partial class Form1 : Form
     {
+        Label[] eticheteNume;
+        Label[] eticheteZiDeNastere;
+        Label[] eticheteNumarDeTelefon;
+        Label[] eticheteGrup;
+        Label[] eticheteEmail;
+        AdministrarePersoane_FisierText adminPersoane;
+        int nrPersoane = 0;
+        ArrayList grupuriSelectate = new ArrayList();
         public Form1()
         {
             InitializeComponent();
@@ -25,39 +35,50 @@ namespace InterfataUtilizator_Agenda
             // setare locatie fisier in directorul corespunzator solutiei
             // astfel incat datele din fisier sa poata fi utilizate si de alte proiecte
             string caleCompletaFisier = locatieFisierSolutie + "\\" + numeFisier;
-            AdministrarePersoane_FisierText adminPersoane = new
+            adminPersoane = new
             Agenda.AdministrarePersoane_FisierText(caleCompletaFisier);
-            int nrPersoane = 0;
-            Persoana[] persoane = adminPersoane.GetPersoane(out nrPersoane);
-           
-            Label[] eticheteNume = new Label[nrPersoane];
-            Label[] eticheteZiDeNastere = new Label[nrPersoane];
-            Label[] eticheteNumarDeTelefon = new Label[nrPersoane];
-            Label[] eticheteGrup = new Label[nrPersoane];
-            Label[] eticheteEmail = new Label[nrPersoane];
 
+            this.Size = new Size(600, 400);
+            dateTimeZiDeNastere.Format = DateTimePickerFormat.Long;
+        }
+        private void AfiseazaPersoane()
+        {
+            this.Width = 1200;
+            adminPersoane.GetPersoane(out nrPersoane);
+            eticheteNume = new Label[nrPersoane];
+            eticheteZiDeNastere = new Label[nrPersoane];
+            eticheteNumarDeTelefon = new Label[nrPersoane];
+            eticheteEmail = new Label[nrPersoane];
+            eticheteGrup = new Label[nrPersoane];  
+            Persoana[] persoane = adminPersoane.GetPersoane(out nrPersoane);
+            lbITitlu.Visible = true;
+            lblNume.Visible = true;
+            lblZiDeNastere.Visible = true;
+            lblNumarDeTelefon.Visible = true;
+            lblEmail.Visible = true;
+            lblGrup.Visible = true;
             for (int i = 0; i < nrPersoane; i++)
             {
                 eticheteNume[i] = new Label
                 {
                     Text = persoane[i].nume,
-                    ForeColor = Color.DarkOrchid,
+                    ForeColor = Color.LightSeaGreen,
                     Visible = true,
                     Size = new Size(51, 20),
                     TextAlign = ContentAlignment.TopLeft,
                     Enabled = true,
-                    Top = 100+i*20,
-                    Left = 85,
-                };                
+                    Top = 100 + i * 20,
+                    Left = 399,
+                };
                 //this.Controls.Add(eticheteNume[i]);
                 eticheteZiDeNastere[i] = new Label
                 {
-                    Text = persoane[i].ziDeNastere.ToString(),
-                    ForeColor = Color.DarkOrchid,
+                    Text = persoane[i].ziDeNastere.ToString("ddd, dd MMMM yyyy"),
+                    ForeColor = Color.LightSeaGreen,
                     Visible = true,
                     Top = 100 + i * 20,
-                    Left = 204,
-                    Size = new Size(80, 20),
+                    Left = 521,
+                    Size = new Size(140, 20),
                     TextAlign = ContentAlignment.TopLeft,
                     Enabled = true
                 };
@@ -65,10 +86,10 @@ namespace InterfataUtilizator_Agenda
                 eticheteNumarDeTelefon[i] = new Label
                 {
                     Text = persoane[i].numarDeTelefon.ToString(),
-                    ForeColor = Color.DarkOrchid,
+                    ForeColor = Color.LightSeaGreen,
                     Visible = true,
                     Top = 100 + i * 20,
-                    Left = 361,
+                    Left = 679,
                     Size = new Size(80, 20),
                     TextAlign = ContentAlignment.TopLeft,
                     Enabled = true
@@ -77,29 +98,29 @@ namespace InterfataUtilizator_Agenda
                 eticheteEmail[i] = new Label
                 {
                     Text = persoane[i].email,
-                    ForeColor = Color.DarkOrchid,
+                    ForeColor = Color.LightSeaGreen,
                     Visible = true,
                     Top = 100 + i * 20,
-                    Left = 539,
-                    Size = new Size(100, 20),
+                    Left = 854,
+                    Size = new Size(120, 20),
                     TextAlign = ContentAlignment.TopLeft,
                     Enabled = true
                 };
                 //this.Controls.Add(eticheteEmail[i]);
                 eticheteGrup[i] = new Label
                 {
-                    Text = persoane[i]._grup.ToString(),
-                    ForeColor = Color.DarkOrchid,
+                    Text = persoane[i].GrupAsString,
+                    ForeColor = Color.LightSeaGreen,
                     Visible = true,
                     Top = 100 + i * 20,
-                    Left = 654,
-                    Size = new Size(60, 20),
+                    Left = 993,
+                    Size = new Size(100, 20),
                     TextAlign = ContentAlignment.TopLeft,
                     Enabled = true
                 };
                 //this.Controls.Add(eticheteGrup[i]);
             }
-            for(int i = 0; i < nrPersoane; i++)
+            for (int i = 0; i < nrPersoane; i++)
             {
                 this.Controls.Add(eticheteNume[i]);
                 this.Controls.Add(eticheteZiDeNastere[i]);
@@ -108,5 +129,73 @@ namespace InterfataUtilizator_Agenda
                 this.Controls.Add(eticheteGrup[i]);
             }
         }
+
+        private void AscundePersoane()
+        {
+            lbITitlu.Visible = false;
+            lblNume.Visible = false;
+            lblZiDeNastere.Visible = false;
+            lblNumarDeTelefon.Visible = false;
+            lblEmail.Visible = false;
+            lblGrup.Visible = false;
+            for(int i = 0; i < nrPersoane; i++)
+            {
+                this.Controls.Remove(eticheteNume[i]);
+                this.Controls.Remove(eticheteZiDeNastere[i]);
+                this.Controls.Remove(eticheteNumarDeTelefon[i]);
+                this.Controls.Remove(eticheteEmail[i]);
+                this.Controls.Remove(eticheteGrup[i]);
+            }
+        }
+
+        private void CheckGrupuri_CheckedChange(object sender, EventArgs e)
+        {
+            CheckBox checkBox = sender as CheckBox;
+            string grupSelectat = checkBox.Text;
+
+            if(checkBox.Checked == true)
+            {
+                grupuriSelectate.Add(grupSelectat);
+            }
+            else
+            {
+                grupuriSelectate.Remove(grupSelectat);
+            }
+        }
+        
+        private void BtnAfiseaza_Click(object sender, EventArgs e)
+        {
+            AfiseazaPersoane();
+        }
+        private void Form1_Click(object sender, EventArgs e)
+        {
+            AscundePersoane();
+            this.Width = 600;
+        }
+
+        private void BtnAdd_Click(object sender, EventArgs e)
+        {
+            Persoana persoana = new Persoana();
+            Persoana.Grup grup = persoana.GetGrup(string.Join(", ", grupuriSelectate.Cast<string>().ToArray()));
+            persoana.grupuri = grupuriSelectate;
+            persoana.nume = txtAddNume.Text;
+            persoana.ziDeNastere = DateTime.Parse(dateTimeZiDeNastere.Text);
+            persoana.numarDeTelefon = Convert.ToInt32(txtAddNumarDeTelefon.Text);
+            persoana.email = txtAddEmail.Text;
+            persoana._grup = grup;
+            persoana.idPersoana = nrPersoane++;
+            adminPersoane.AddPersoana(persoana);
+            AfiseazaPersoane();
+        }
+
+        private void Lbl_VisibleChanged(object sender, EventArgs e)
+        {
+            Console.WriteLine("Vizibilitatea s-a schimbat!");
+        }
     }
 }
+/*to do:
+ 1. data validation
+ 2. search functions
+ 3. delete function
+ 4. figure out why the calendar isn't changing*/
